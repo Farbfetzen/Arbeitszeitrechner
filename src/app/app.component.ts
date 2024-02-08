e0c41881cb23dfb549b04fd2f1494257b528d3c5
Farbfetzen <farbfetzen@posteo.de>
2024-02-08 21:57:10 +0100

    Create input elements

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,27 @@
 import { Component } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
 @Component({
     selector: "app-root",
     standalone: true,
-    imports: [],
+    imports: [FormsModule],
     templateUrl: "./app.component.html",
 })
-export class AppComponent {}
+export class AppComponent {
+    timeLog = "";
+
+    // Summarize times that belong to the same Jira issue.
+    summarizeForSameJiraIssue = true;
+
+    // Round up times to the nearest 5 minutes.
+    roundUpTo5 = true;
+
+    updateResult(): void {
+        // TODO: Update the output table
+        // console.log(this.timeLog);
+        // console.log("summarize: " + this.summarizeForSameJiraIssue);
+        // console.log("round: " + this.roundUpTo5);
+    }
+
+    // TODO: Der identifier für Jira-Issues heißt "key". Also "ABC-123" ist der key.
+}
