6f4343259059f255eb04f968fd7eb4a8ae6cfdc0
Farbfetzen <farbfetzen@posteo.de>
2024-02-09 21:11:28 +0100

    Improve the layout

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,8 @@ export class AppComponent {
     // Round up times to the nearest 5 minutes.
     roundUpTo5 = true;
 
+    result = "";
+
     updateResult(): void {
         // TODO: Update the output table
         // console.log(this.timeLog);
@@ -25,3 +27,20 @@ export class AppComponent {
 
     // TODO: Der identifier für Jira-Issues heißt "key". Also "ABC-123" ist der key.
 }
+
+/*
+TODO: remove this
+Test data
+
+08:35:00 ABC-123 Make some coffee.
+08:44:21 FOO-9999 Review a PR.
+09:23:57 Pause
+09:27:03 SOMETHING-777 Meeting.
+11:03:22 CODE-42 Write some code.
+12:15:01 Pause
+12:59:31 INTERNAL-5555 Research something.
+14:22:23 FOO-9999 Review again.
+15:00:00 CODE-42 Finally back to programming.
+16:55:12 INTERNAL-5556 Sort e-mails, log time.
+17:10:42
+*/
